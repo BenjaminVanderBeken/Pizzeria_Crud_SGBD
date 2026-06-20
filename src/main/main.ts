6d350943531133ad3d79f182bf12afa31ea34e05
BenjaminVanderBeken <benjamin.vander.beken@hotmail.com>
2026-06-20 15:31:03 +0200

    Utiliser loadFile avec le build Angular au lieu du dev server Vite

--- src/main/main.ts
+++ src/main/main.ts
@@ -19,14 +19,9 @@ const createWindow = () => {
     },
   });
 
-  // and load the index.html of the app.
-  if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
-    mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
-  } else {
-    mainWindow.loadFile(
-      path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`),
-    );
-  }
+  mainWindow.loadFile(
+    path.join(__dirname, '..', '..', 'renderer/app/dist/app/browser/index.html'),
+  );
 
   if (!app.isPackaged) {
     mainWindow.webContents.openDevTools();
